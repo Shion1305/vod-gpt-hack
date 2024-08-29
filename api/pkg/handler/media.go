b8d22a71c4162457f6cfecc087a388b0260c00ba
pirotyyy <hiroki1423@docomo.ne.jp>
2024-08-29 22:40:03 +0900

    :sparkles: MP4関連のHandler実装

---
+++ api/pkg/handler/media.go
@@ -0,0 +1,99 @@
+package handler
+
+import (
+	"api/pkg/config"
+	"api/pkg/domain"
+	infraDynamo "api/pkg/infra/dynamo"
+	infraS3 "api/pkg/infra/s3"
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type MediaHandler struct {
+	s *infraS3.S3
+	d *infraDynamo.Dynamo
+}
+
+func NewMediaHandler(s *infraS3.S3, d *infraDynamo.Dynamo) MediaHandler {
+	return MediaHandler{
+		s,
+		d,
+	}
+}
+
+func (m MediaHandler) Create() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := uuid.New().String()
+		media := domain.Media{
+			ID: id,
+		}
+		item, err := attributevalue.MarshalMap(media)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+		}
+		_, err = m.d.Client.PutItem(context.TODO(), &dynamodb.PutItemInput{
+			TableName: aws.String("media"), Item: item,
+		})
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+		}
+		c.JSON(http.StatusCreated, gin.H{
+			"id": id,
+		})
+	}
+}
+
+func (m MediaHandler) UploadMP4() gin.HandlerFunc {
+	conf := config.Get()
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		if id == "" {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": "Missing 'id' url parameter.",
+			})
+		}
+
+		file, fileHeader, err := c.Request.FormFile("file")
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		defer file.Close()
+
+		contentType := fileHeader.Header.Get("Content-Type")
+		if contentType == "" {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": "Missing Content-Type Header.",
+			})
+		}
+
+		input := &s3.PutObjectInput{
+			Bucket:        aws.String(conf.Infrastructure.S3.Bucket),
+			Key:           aws.String(fmt.Sprintf("%s/%s.mp4", id, uuid.New().String())),
+			Body:          file,
+			ContentLength: aws.Int64(fileHeader.Size),
+			ContentType:   aws.String(contentType),
+		}
+
+		_, err = m.s.Client.PutObject(c, input)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+
+		c.JSON(http.StatusCreated, gin.H{
+			"message": "upload mp4 file successfully.",
+		})
+	}
+}
