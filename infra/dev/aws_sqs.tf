03ad621c9e0f46cf21db3925d00c37b9d83bbfba
Shion Ichikawa <shion1305@gmail.com>
2024-09-01 01:36:47 +0900

    🔧 sqs name_prefix

--- infra/dev/aws_sqs.tf
+++ infra/dev/aws_sqs.tf
@@ -1,5 +1,5 @@
 resource "aws_sqs_queue" "transcribe-sqs" {
-  name                        = "${local.prefix}-transcribe-sqs"
+  name_prefix                 = "${local.prefix}-transcribe-sqs"
   fifo_queue                  = true
   content_based_deduplication = true
 }
