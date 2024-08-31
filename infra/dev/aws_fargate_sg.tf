9c36f4b0b6984bef7d2db77f2c1523cd97e3144a
Shion Ichikawa <shion1305@gmail.com>
2024-08-31 23:07:31 +0900

    🚨 run terragrunt fmt

--- infra/dev/aws_fargate_sg.tf
+++ infra/dev/aws_fargate_sg.tf
@@ -2,16 +2,16 @@ resource "aws_security_group" "external_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -20,16 +20,16 @@ resource "aws_security_group" "fargate_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
