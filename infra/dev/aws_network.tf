4cd249c8bb0fe227a7b6703ed3d01374b5511995
Shion Ichikawa <shion1305@gmail.com>
2024-08-31 21:51:57 +0900

    ✨ subnet2

--- infra/dev/aws_network.tf
+++ infra/dev/aws_network.tf
@@ -17,6 +17,16 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+resource "aws_subnet" "public_subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-west-2b"
+  tags = {
+    Name = "public-subnet2"
+  }
+}
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = {
