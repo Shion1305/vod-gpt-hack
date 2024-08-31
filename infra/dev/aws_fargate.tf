be1c3f18f4ebf243fe66d9b2ce40cba2613cd492
Shion Ichikawa <shion1305@gmail.com>
2024-08-31 23:16:26 +0900

    🐛 fix container port in ecs_service

--- infra/dev/aws_fargate.tf
+++ infra/dev/aws_fargate.tf
@@ -22,7 +22,7 @@ resource "aws_ecs_service" "fargate_service" {
   load_balancer {
     target_group_arn = aws_lb_target_group.tg.arn
     container_name   = "api-main-container"
-    container_port   = 80
+    container_port   = 8080
   }
 }
 
