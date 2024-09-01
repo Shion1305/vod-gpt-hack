f236456ad33e7ff3c910a07d4cbd40f84d4b4fb8
pirotyyy <hiroki1423@docomo.ne.jp>
2024-09-01 14:36:23 +0900

    list

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -3,8 +3,8 @@ import VideoCard from "./VideoCard";
 
 const Header = () => {
   return (
-    <div className="w-screen h-16 bg-black flex items-center">
-      <p className="text-white text-2xl ml-8 shadow-xl">Teacher</p>
+    <div className="w-screen h-16 bg-black flex items-center shadow-xl">
+      <p className="text-white text-2xl ml-8 ">まなびでお</p>
     </div>
   );
 };
