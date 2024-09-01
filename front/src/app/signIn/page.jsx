7d0dd57e45deed9797d3d544a2dae39ec76f9376
koishichito <118346971+koishichito@users.noreply.github.com>
2024-09-01 12:33:12 +0900

    auth

--- front/src/app/signIn/page.jsx
+++ front/src/app/signIn/page.jsx
@@ -1,76 +1,80 @@
 'use client'
 
-// import styles from '../styles/Home.module.css'
-// 現時点で使わないものもあるが今後のことを考えて入れておく
-import { Col, Container, Form, FormGroup, Input, Label, Row, Button } from "reactstrap";
 import { useEffect, useState } from 'react';
-// supabase
-import { supabase } from '../utils/supabase';
+import { supabase} from '../../utils/supabase'
+import { useRouter } from "next/navigation";
+import * as React from "react"
 
-// ヘッダーコンポーネントをインポート
-import  Header from '../components/Header';
-// useRouterをインポート
-import { useRouter } from 'next/router';
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export default function Register() {
-  // useStateでユーザーが入力したメールアドレスとパスワードをemailとpasswordに格納する
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const router = useRouter();
 
-  // supabaseのユーザー登録の関数
-  const doLogin =  async () => {
-    // supabaseで用意されているユーザー登録の関数
-    const { data, error } = await supabase.auth.signInWithPassword({ email, password })
-    if (error) throw new Error(error.message)
-    console.log(data)
-    // ログインを反映させるためにリロードさせる
-    router.reload()
-  }
+  const doLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+    if (error) throw new Error(error.message);
+    console.log(data);
+    router.reload();
+  };
 
   return (
-    // Home.module.cssでcardクラスに適用されているCCSを、このdivタグに適用する
-    <div className={styles.card}>
-      <h1>ログイン</h1>
-      <Header/>
-      <div>
-        <Form>
-            <FormGroup>
-              <Label>
-                メールアドレス：
-              </Label>
-              <Input
-                type="email"
-                name="email"
-                style={{ height: 50, fontSize: "1.2rem" }}
-                // onChangeでユーザーが入力した値を取得し、その値をemailに入れる
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label>
-                パスワード：
-              </Label>
-              <Input
-                type="password"
-                name="password"
-                style={{ height: 50, fontSize: "1.2rem" }}
-                // onChangeでユーザーが入力した値を取得し、その値をpasswordに入れる
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </FormGroup>
-            <Button
-                style={{ width: 220 }}
-                color="primary"
-                // 登録ボタンがクリックされたとき関数が実行されるようにする
-                onClick={()=>{
-                  doLogin();
-                }}
-              >
-              ログイン
-            </Button>
-        </Form>
-      </div>
+    <div className="w-screen h-screen flex items-center justify-center bg-brack">
+
+    <Card className="w-[40%]">
+      <CardHeader>
+        <CardTitle>サインイン</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Framework</Label>
+              <Select>
+                <SelectTrigger id="framework">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="next">Next.js</SelectItem>
+                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                  <SelectItem value="astro">Astro</SelectItem>
+                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
     </div>
   )
 }
+
+
